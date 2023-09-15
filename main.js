0a1225af05e241006d21e5d6e09578a15a074ca9
Smile <singhswapnil060303@gmail.com>
2023-09-16 00:02:04 +0530

    updated code

---
+++ main.js
@@ -0,0 +1,5 @@
+// localStream :will be for local camera video feed abd micro phone audio
+let localStream;
+// once we are connented to another user this will be their camera and audio data 
+let remoteStream;
+
