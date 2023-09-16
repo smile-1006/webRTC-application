5597760821b49da763152e7b6fb9e61dd948b0c8
Smile <singhswapnil060303@gmail.com>
2023-09-16 10:35:56 +0530

    updating with video feed in project

--- main.js
+++ main.js
@@ -3,3 +3,31 @@ let localStream;
 // once we are connented to another user this will be their camera and audio data 
 let remoteStream;
 
+let init = async() => {
+    // this will request our  camera feeds and audio feeds from the user
+    localStream= await navigator.mediaDevices.getUserMedia({video:true, audio:false})
+    //document.getElementById("user-1").srcObject =localStream
+
+    const videoElement = document.getElementById("user-1");
+    videoElement.srcObject= localStream;
+
+    videoElement.onplay();
+}
+
+init()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
