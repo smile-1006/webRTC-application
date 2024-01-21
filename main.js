5f5095193ef8da52e1e930b670771a4ad524aab7
Smile <singhswapnil060303@gmail.com>
2024-01-21 23:15:32 +0530

    realtime messaging using agoraRTM.

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 let APP_ID ="c853791b139f4f66af3ef4b7c68499a6"
 
 let token =null;
-let uid = math.floor(Math.random()* 10000)
+let uid = String(Math.floor(Math.random()* 10000))
+
+let client ;
+let channel;
 
 // localStream :will be for local camera video feed abd micro phone audio
 let localStream;
@@ -19,13 +22,24 @@ const servers = {
 }
 
 let init = async() => {
+    
+    client = await AgoraRTM.createInstance(APP_ID)
+    await client.login({uid, token})
     // this will request our  camera feeds and audio feeds from the user
+    channel = await client.createChannel('main')
+    await channel.join()    
+    
     localStream= await navigator.mediaDevices.getUserMedia({video:true, audio:false})
     document.getElementById("user-1").srcObject =localStream
 
     createOffer();
     
-}
+};
+
+let handleUserJoined = async (MemberId) => {
+    console.log("User Joined", MemberId);
+    
+};
 
 let createOffer = async () => {
     // creating new peer connection and this interface that store all the information between us and that remote peer and that provide us with the bounch of mathods to actually connect to the banch of peer
@@ -39,7 +53,7 @@ let createOffer = async () => {
         peerConnection.addTrack(track, localStream)
     })
 
-    //lesion when peer are connected with easch other
+    //lession when peer are connected with easch other
     peerConnection.ontrack = (event ) => {
         event.streams[0].getTracks().forEach((track) =>{
             remoteStream.addTrack(track)
@@ -67,6 +81,7 @@ let createOffer = async () => {
     
     //lets create an offer
     let offer = await peerConnection.createOffer()
+    //it will treeger the onicecandidate after the offer
     await peerConnection.setLocalDescription(offer)
 
     console.log("offer:",offer);
